590d9212c6ca732c6a1252304723a2f6caf7e873
Nick Lentsch <nick@launchjourney.com>
2017-08-07 13:11:34 -0400

    chapter 2 task 2 completed

--- src/app/2-add-logic/controllers/2-choose-an-object-for-the-process.controller.js
+++ src/app/2-add-logic/controllers/2-choose-an-object-for-the-process.controller.js
@@ -1,40 +1,41 @@
 'use strict';
 
-export default function ChooseAnObjectForTheProcessController($scope, $timeout, Steps, WizardHandler, Drawer, $state) {
-  $scope.ChooseAnObjectForTheProcess = {
-  };
-
-  $scope.ChooseAnObjectForTheProcess.sortableOptions = {
-    update() {
-      if (WizardHandler.wizard('monitor').currentStepNumber() === 2) {
-        WizardHandler.wizard('monitor').next();
-        Steps.activate('three');
-      }
-    },
-    axis: 'y'
-  };
-
-  $scope.ChooseAnObjectForTheProcess.fields = [
-    {
-      name: 'First Name',
-      required: true
-    },
-    {
-      name: 'Last Name',
-      required: true
-    },
-    {
-      name: 'Company',
-      required: false
-    },
-    {
-      name: 'Email (a)',
-      required: true
-    }
+export default function ChooseAnObjectForTheProcessController($scope, $timeout, Steps, WizardHandler) {
+  $scope.processOptions = [
+    {label: 'Find an object...', value: 1},
+    {label: 'Asset', value: 2},
+    {label: 'Customer Feedback', value: 3},
+    {label: 'Assistant Progress', value: 4},
+    {label: 'Campaign', value: 5},
+    {label: 'Campaign Member', value: 6}
   ];
 
+  $scope.process = {
+    name: '',
+    starts: $scope.processOptions[0]
+  };
+
+  $scope.$watch('process.starts', (newValue, oldValue) => {
+    if (newValue !== oldValue) {
+      if (newValue.value === 3) {
+        Steps.activate('three');
+        if (WizardHandler.wizard('monitor').currentStepNumber() === 3) {
+          WizardHandler.wizard('monitor').next();
+        }
+      }
+    }
+  });
+
   $scope.Next = function () {
-    Steps.clear();
-    $state.go('add-criteria');
+    if (WizardHandler.wizard('monitor').currentStepNumber() === 1) {
+      WizardHandler.wizard('monitor').next();
+      Steps.activate('one');
+    } else if (WizardHandler.wizard('monitor').currentStepNumber() === 2) {
+      WizardHandler.wizard('monitor').next();
+      Steps.activate('two');
+    } else if (WizardHandler.wizard('monitor').currentStepNumber() === 3) {
+      WizardHandler.wizard('monitor').next();
+      Steps.activate('three');
+    }
   };
 }
--- src/app/2-add-logic/controllers/2-choose-an-object-for-the-process.drawer.controller.js
+++ src/app/2-add-logic/controllers/2-choose-an-object-for-the-process.drawer.controller.js
@@ -12,48 +12,38 @@ export default function ChooseAnObjectForTheProcessDrawerController($scope, Wiza
     Steps.clear();
     Steps.pop({
       number: 'one',
-      title: 'Click &lsquo;Next&rsquo; to start setting up your form.'
+      title: 'Click on \'Add Object\''
     });
 
     Steps.pop({
       number: 'two',
-      title: 'Click and drag the form fields into the order that you prefer.'
+      title: 'Choose \'Customer Feedback\' from the dropdown choices.'
     });
 
     Steps.pop({
       number: 'three',
-      title: 'Click &lsquo;Next&rsquo; to learn how to add completion actions to your form.'
-    });
-
-    Steps.pop({
-      number: 'four',
-      title: 'Use the blank dropdown menu to view all the possible completion actions that can be triggered after a form submission. See some unfamiliar terminology? Don\'t worry &mdash; we\'ll cover everything by the end of our interactive tour.'
-    });
-
-    Steps.pop({
-      number: 'five',
-      title: 'Click &rsquo;Confirm &amp; Save&lsquo; to advance to the Landing Page Builder.'
+      title: 'We want our process to be activated when we create a new piece of Customer Feedback object so we\'ll leave the settings as they are. <br/> Click \'Save.\''
     });
 
     Hotspots.clear();
-    Hotspots.pop({
-      number: 2,
-      position: {
-        left: '389px',
-        top: '363px'
-      }
-    });
-
-    Hotspots.pop({
-      number: 1,
-      position: {
-        left: '694px',
-        top: '168px'
-      }
-    });
+    // Hotspots.pop({
+    //   number: 2,
+    //   position: {
+    //     left: '389px',
+    //     top: '363px'
+    //   }
+    // });
+    //
+    // Hotspots.pop({
+    //   number: 1,
+    //   position: {
+    //     left: '694px',
+    //     top: '168px'
+    //   }
+    // });
 
     $timeout(() => {
-      Steps.activate('two');
+      Steps.activate('one');
     }, 1000);
   };
 }
