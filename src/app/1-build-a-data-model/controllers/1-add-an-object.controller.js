edc9454c146c1ad1f937db6b66a3cd1c7f322b1d
Nick Lentsch <nick@launchjourney.com>
2017-08-09 16:42:25 -0400

    chapter 1 task 1

--- src/app/1-build-a-data-model/controllers/1-add-an-object.controller.js
+++ src/app/1-build-a-data-model/controllers/1-add-an-object.controller.js
@@ -1,49 +1,55 @@
 'use strict';
 
-export default function AddAnObjectController($scope, Steps, Hotspots, WizardHandler, $timeout, UserProfile, TopNavbar) {
-  $scope.campaign = {
-    name: '',
-    cost: ''
+export default function AddAnObjectController($scope, Steps, Hotspots, WizardHandler, $timeout) {
+  $scope.customobj = {
+    name: ''
   };
 
-  const delayInMs = 1000;
+  $scope.button = {
+    show: false
+  };
 
-  let timeoutPromiseCampaignName;
-  $scope.$watch('campaign.name', (newValue, oldValue) => {
-    if (oldValue !== newValue) {
-      $timeout.cancel(timeoutPromiseCampaignName);
-      timeoutPromiseCampaignName = $timeout(() => {
-        if (newValue.length > 3) {
-          Steps.activate('two');
-          if (WizardHandler.wizard('monitor').currentStepNumber() === 2) {
-            WizardHandler.wizard('monitor').next();
-          }
-        }
-      }, delayInMs);
-    }
-  });
+  $scope.label = {
+    active: true
+  };
 
-  let timeoutPromiseCampaignCost;
-  $scope.$watch('campaign.cost', (newValue, oldValue) => {
-    if (oldValue !== newValue) {
-      $timeout.cancel(timeoutPromiseCampaignCost);
-      timeoutPromiseCampaignCost = $timeout(() => {
-        Steps.activate('three');
-        if (WizardHandler.wizard('monitor').currentStepNumber() === 3) {
+  $scope.ToggleButton = () => {
+    $scope.button.show = !$scope.button.show;
+  };
+
+  $scope.toggleLabel = () => {
+    $scope.label.active = !$scope.label.active;
+  };
+
+  $scope.$watch('customobj.name', (newValue, oldValue) => {
+    if (newValue !== oldValue) {
+      if (newValue.toLowerCase() === 'customer feedback') {
+        $scope.toggleLabel();
+        $timeout(() => {
           WizardHandler.wizard('monitor').next();
-        }
-      }, delayInMs);
+          Steps.activate('four');
+        }, 300);
+      }
     }
   });
 
-  $scope.CreateCampaign = function () {
-    if (WizardHandler.wizard('monitor').currentStepNumber() === 4) {
-      UserProfile.CampaignName = $scope.campaign.name;
-      Steps.clear();
-      Hotspots.clear();
-      TopNavbar.HotspotsCount = 0;
-      TopNavbar.HotspotsEnabled = false;
+  $scope.Next = function () {
+    // $log.log('Current step: ' + WizardHandler.wizard('monitor').currentStepNumber());
+    if (WizardHandler.wizard('monitor').currentStepNumber() === 1) {
       WizardHandler.wizard('monitor').next();
+      Steps.activate('one');
+    } else if (WizardHandler.wizard('monitor').currentStepNumber() === 2) {
+      WizardHandler.wizard('monitor').next();
+      Steps.activate('two');
+    } else if (WizardHandler.wizard('monitor').currentStepNumber() === 3) {
+      WizardHandler.wizard('monitor').next();
+      Steps.activate('three');
+    } else if (WizardHandler.wizard('monitor').currentStepNumber() === 4) {
+      $scope.toggleLabel();
+      $timeout(() => {
+        WizardHandler.wizard('monitor').next();
+        Steps.activate('four');
+      }, 300);
     }
   };
 }
