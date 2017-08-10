6f74fd9a6ef724d2ae9e0325d46ef27c4a659ebb
Nick Lentsch <nick@launchjourney.com>
2017-08-10 15:04:50 -0400

    chapter 1 task 2

--- src/app/1-build-a-data-model/controllers/2-add-an-external-object.controller.js
+++ src/app/1-build-a-data-model/controllers/2-add-an-external-object.controller.js
@@ -17,12 +17,30 @@ export default function AddAnExternalObjectController($scope, Steps, Hotspots, W
     active: false
   };
 
+  $scope.step4 = {
+    box1: true,
+    box2: false,
+    box3: false
+  };
+
   $scope.toggleScroll = () => {
     $scope.input.active = !$scope.input.active;
   };
 
   $scope.toggleBkg = () => {
     $scope.bkg.active = !$scope.bkg.active;
+  };
+
+  $scope.toggleBox2 = () => {
+    $scope.step4.box1 = false;
+    $scope.step4.box2 = true;
+    $scope.step4.box3 = false;
+  };
+
+  $scope.toggleBox3 = () => {
+    $scope.step4.box1 = false;
+    $scope.step4.box2 = false;
+    $scope.step4.box3 = true;
   };
 
   $scope.$watch('customobj.name', (newValue, oldValue) => {
@@ -59,6 +77,9 @@ export default function AddAnExternalObjectController($scope, Steps, Hotspots, W
     } else if (WizardHandler.wizard('monitor').currentStepNumber() === 4) {
       WizardHandler.wizard('monitor').next();
       Steps.activate('four');
+    } else if (WizardHandler.wizard('monitor').currentStepNumber() === 5) {
+      WizardHandler.wizard('monitor').next();
+      Steps.activate('five');
     }
   };
 }
