106aead31795fc14d70b71243fdf0d6628fbe5b8
Nick Lentsch <nick@launchjourney.com>
2017-08-15 16:37:10 -0400

    Chapter 3 Task 4 ready for drag and drop

--- src/app/3-build-your-app/controllers/4-add-custom-components.drawer.controller.js
+++ src/app/3-build-your-app/controllers/4-add-custom-components.drawer.controller.js
@@ -2,35 +2,23 @@
 
 export default function AddCustomComponentsDrawerController($scope, WizardHandler, TopNavbar, Steps, Hotspots, Drawer, $timeout) {
   TopNavbar.InfoActive = true;
+  Drawer.openToIntro();
 
   $scope.beginStory = function () {
     Drawer.close();
+    WizardHandler.wizard('monitor').next();
     TopNavbar.InfoActive = false;
 
     Steps.clear();
     Steps.pop({
       number: 'one',
-      title: 'Choose a list of email recipients using the dropdown. You may also choose a different sender for each version of your email.'
+      title: ''
     });
 
-    Steps.pop({
-      number: 'two',
-      title: 'When you are finished, click &ldquo;Save&rdquo; (note: no email inboxes will be harmed in the sending of this email).'
-    });
+    Hotspots.clear();
 
     $timeout(() => {
       Steps.activate('one');
-      Hotspots.clear();
-
-      Hotspots.pop({
-        number: 1,
-        position: {
-          left: '24px',
-          top: '120px'
-        }
-      });
     }, 1000);
   };
-
-  $scope.beginStory();
 }
