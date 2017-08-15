1f1091d6a7a5ab358f8b42101e81b03ead89f49e
Nick Lentsch <nick@launchjourney.com>
2017-08-15 14:14:48 -0400

    Chapter 3 Task 3 Initial Check In

--- src/app/3-build-your-app/controllers/3-create-an-app-from-base-and-custom-lightning-components.controller.js
+++ src/app/3-build-your-app/controllers/3-create-an-app-from-base-and-custom-lightning-components.controller.js
@@ -1,26 +1,38 @@
 'use strict';
 
-export default function CreateAnAppFromBaseAndCustomLightningComponentsController($scope, WizardHandler, Steps, $timeout) {
-  $scope.preview = {
-    iPhoneSelected: false
+export default function CreateAnAppFromBaseAndCustomLightningComponentsController($scope, WizardHandler, Steps) {
+  $scope.search = {
+    input: ''
   };
 
-  $scope.SelectDevice = function () {
-    $scope.preview.iPhoneSelected = true;
+  $scope.input = {
+    active: true
+  };
 
-    Steps.activate('two');
-
-    $timeout(() => {
-      if (WizardHandler.wizard('monitor').currentStepNumber() === 2) {
+  $scope.$watch('search.input', (newValue, oldValue) => {
+    if (newValue !== oldValue) {
+      if (newValue.toLowerCase() === 'ideas') {
         WizardHandler.wizard('monitor').next();
+        Steps.activate('three');
       }
-    }, 1000);
-  };
+    }
+  });
 
-  $scope.EmailPreviewSuccess = function () {
-    $timeout(() => {
-      Steps.clear();
-    }, 0);
+  $scope.Next = function () {
+    // $log.log('Current step: ' + WizardHandler.wizard('monitor').currentStepNumber());
+    if (WizardHandler.wizard('monitor').currentStepNumber() === 1) {
+      WizardHandler.wizard('monitor').next();
+      Steps.activate('one');
+    } else if (WizardHandler.wizard('monitor').currentStepNumber() === 2) {
+      WizardHandler.wizard('monitor').next();
+      Steps.activate('two');
+    } else if (WizardHandler.wizard('monitor').currentStepNumber() === 3) {
+      WizardHandler.wizard('monitor').next();
+      Steps.activate('three');
+    } else if (WizardHandler.wizard('monitor').currentStepNumber() === 4) {
+      WizardHandler.wizard('monitor').next();
+      Steps.activate('four');
+    }
   };
 }
 
