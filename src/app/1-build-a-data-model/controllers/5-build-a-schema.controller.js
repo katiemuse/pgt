91f1b2fac187228ea7ecc623398d9f42330f946d
Nick Lentsch <nick@launchjourney.com>
2017-08-15 10:41:25 -0400

    Chapter 1 Task 5

--- src/app/1-build-a-data-model/controllers/5-build-a-schema.controller.js
+++ src/app/1-build-a-data-model/controllers/5-build-a-schema.controller.js
@@ -1,35 +1,52 @@
 'use strict';
 
-export default function BuildASchemaController($scope, Steps, Hotspots, WizardHandler, $timeout) {
-  $scope.customobj = {
-    name: ''
+export default function BuildASchemaController($scope, Steps, Hotspots, WizardHandler) {
+  $scope.check = {
+    cf: false,
+    account: false,
+    order: false,
+    product: false
   };
 
-  $scope.button = {
-    show: false
-  };
-
-  $scope.label = {
+  $scope.input = {
     active: true
   };
 
-  $scope.ToggleButton = () => {
-    $scope.button.show = !$scope.button.show;
+  $scope.select = {
+    active: true
   };
 
-  $scope.toggleLabel = () => {
-    $scope.label.active = !$scope.label.active;
+  $scope.orderOptions = [
+    {label: '--None--', value: 1},
+    {label: 'Customer Feedback', value: 2}
+  ];
+
+  $scope.order = {
+    input: '',
+    select: $scope.orderOptions[0]
   };
 
-  $scope.$watch('customobj.name', (newValue, oldValue) => {
+  $scope.$watchGroup(['check.cf', 'check.account', 'check.order', 'check.product'], newValues => {
+    const isTrue = newValues.every(val => {
+      return val === true;
+    });
+    if (isTrue) {
+      WizardHandler.wizard('monitor').next();
+      Steps.activate('two');
+    }
+  });
+
+  $scope.$watch('order.input', (newValue, oldValue) => {
     if (newValue !== oldValue) {
-      if (newValue.toLowerCase() === 'customer feedback') {
-        $scope.toggleLabel();
-        $timeout(() => {
-          WizardHandler.wizard('monitor').next();
-          Steps.activate('four');
-        }, 600);
+      if (newValue.toLowerCase() === 'order details') {
+        $scope.input.active = false;
       }
+    }
+  });
+
+  $scope.$watch('order.select', newValue => {
+    if (Number(newValue.value) === 2) {
+      $scope.select.active = false;
     }
   });
 
@@ -45,11 +62,11 @@ export default function BuildASchemaController($scope, Steps, Hotspots, WizardHa
       WizardHandler.wizard('monitor').next();
       Steps.activate('three');
     } else if (WizardHandler.wizard('monitor').currentStepNumber() === 4) {
-      $scope.toggleLabel();
-      $timeout(() => {
-        WizardHandler.wizard('monitor').next();
-        Steps.activate('four');
-      }, 600);
+      WizardHandler.wizard('monitor').next();
+      Steps.activate('four');
+    } else if (WizardHandler.wizard('monitor').currentStepNumber() === 5) {
+      WizardHandler.wizard('monitor').next();
+      Steps.activate('five');
     }
   };
 }
