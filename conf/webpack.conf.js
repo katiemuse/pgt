4f0258d860c63dd839c3bd9520d2b5f2035fe8ab
Adam Scott <adam@launchjourney.com>
2017-08-15 15:17:19 -0400

    adding drag and drop - add pace (didn't enable it)

--- conf/webpack.conf.js
+++ conf/webpack.conf.js
@@ -82,8 +82,7 @@ module.exports = {
       jquery: 'jquery',
       "window.jQuery": "jquery",
       Tether: 'tether',
-      'window.Tether': 'tether',
-      pace: 'pace-progress'
+      'window.Tether': 'tether'
     })
   ],
   devtool: 'source-map',
