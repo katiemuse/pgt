87efe96ae8f31e5f7e4a24e9223ffdb6b94c7ace
Adam Scott <adam@launchjourney.com>
2017-10-16 16:29:42 -0400

    SGT-151 Chapter 1 - Task 3 (Einstein Builder) - The Step 6 instructions don't match what is on the screen.

--- src/app/1-build-a-data-model/controllers/3-einstein-builder.drawer.controller.js
+++ src/app/1-build-a-data-model/controllers/3-einstein-builder.drawer.controller.js
@@ -51,7 +51,7 @@ export default function AddACustomFieldDrawerController(
     Steps.pop({
       number: 'six',
       title:
-        'Select \'Predicted NPS Score\' as a field into which the Einstein models will push your prediction and will update it every hour. Click  \'Next\'.'
+        'Select \'NPS Score\' as a field into which the Einstein models will push your prediction and will update it every hour. Click  \'Next\'.'
     });
 
     Hotspots.clear();
