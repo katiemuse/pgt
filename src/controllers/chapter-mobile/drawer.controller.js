ad97457ac002af737028a437a98af4a3cd33a8c5
Roger Peters <roger@launchxd.com>
2019-10-24 12:13:25 -0400

    making decent progress

--- src/controllers/chapter-mobile/drawer.controller.js
+++ src/controllers/chapter-mobile/drawer.controller.js
@@ -1,19 +1,27 @@
 'use strict';
 
-export default function ChapterMobileDrawerController($scope, WizardHandler, TopNavbar, Steps, Hotspots, Drawer, $timeout) {
+export default function ChapterMobileDrawerController($scope, WizardHandler, TopNavbar, Steps, Hotspots, Drawer, $timeout, $rootScope) {
   TopNavbar.InfoActive = true;
   Drawer.openToIntro();
 
   $scope.beginStory = function () {
     Drawer.close();
-    WizardHandler.wizard('phone').next();
+    // WizardHandler.wizard('monitor').next();
+    WizardHandler.wizard("phone").goTo(1);
 
     Steps.clear();
+    Steps.pop({
+      number: "one",
+      title: "Click &rdquo;100 kWh&ldquo; to upgrade the battery."
+    });
 
-    Hotspots.clear();
+    Steps.pop({
+      number: "two",
+      title: "Step 2 for chapter-mobile"
+    });
 
-    $timeout(() => {
-      Steps.activate('one');
-    }, 1000);
+    // Hotspots.clear();
+
+    $rootScope.showMobilePopout = true;
   };
 }
--- src/controllers/chapter-mobile/phone.controller.js
+++ src/controllers/chapter-mobile/phone.controller.js
@@ -1,59 +1,19 @@
 'use strict';
 
 export default function ChapterMobilePhoneController($scope, $timeout, Steps, WizardHandler, $log) {
-  $scope.beginStory = function() {
-    WizardHandler.wizard("phone").goTo(1);
-
-    Steps.clear();
-    Steps.pop({
-      number: "one",
-      title: "Click &lsquo;Get more on the App Exchange&rsquo; "
-    });
-
-    Steps.pop({
-      number: "two",
-      title: "In the search bar, type &lsquo;ideas&rsquo; and hit enter"
-    });
-
-    Steps.pop({
-      number: "three",
-      title: "Select &lsquo;Community Ideas&rsquo; from the search results"
-    });
-
-    Steps.pop({
-      number: "four",
-      title: "Click &lsquo;Get It Now&rsquo;"
-    });
-
-    Steps.pop({
-      number: "five",
-      title: "Drag and drop the Customer Ideas component from the AppExchange below the Sales Order Chart"
-    });
-  };
-
   $scope.Next = function () {
-    $log.log('Current step: ' + WizardHandler.wizard('phone').currentStepNumber());
+    $log.log('Current wizard step: ' + WizardHandler.wizard('phone').currentStepNumber());
+
     if (WizardHandler.wizard('phone').currentStepNumber() === 0) {
       $log.log('A');
       WizardHandler.wizard('phone').next();
+      Steps.activate('');
     } else if (WizardHandler.wizard('phone').currentStepNumber() === 1) {
       $log.log('B');
       WizardHandler.wizard('phone').next();
       Steps.activate('one');
     } else if (WizardHandler.wizard('phone').currentStepNumber() === 2) {
       $log.log('C');
-      WizardHandler.wizard('phone').next();
-      Steps.activate('two');
-    } else if (WizardHandler.wizard('phone').currentStepNumber() === 3) {
-      $log.log('D');
-      WizardHandler.wizard('phone').next();
-      Steps.activate('three');
-    } else if (WizardHandler.wizard('phone').currentStepNumber() === 4) {
-      $log.log('E');
-      WizardHandler.wizard('phone').next();
-      Steps.activate('four');
-    } else {
-      $log.log('F');
       WizardHandler.wizard('phone').next();
       Steps.activate('one');
     }
