90dd0ae047776342e2fa096277aae765d145bbd4
Roger Peters <roger@launchxd.com>
2019-10-30 10:17:07 -0400

    Fixed error showing up in console from logic watching a field not first validating that the field exists before checking its length

--- src/controllers/chapter-flow/desktop.controller.js
+++ src/controllers/chapter-flow/desktop.controller.js
@@ -10,7 +10,7 @@ export default function ChapterFlowController($scope, Steps, Hotspots, WizardHan
   $scope.$watch("label.input", newValue => {
     $timeout.cancel(timeoutExtdsName);
     timeoutExtdsName = $timeout(() => {
-      if (newValue.length > 2) {
+      if (angular.isDefined(newValue) && newValue.length > 2) {
         $scope.goToStep4();
       }
     }, delayInMs);
