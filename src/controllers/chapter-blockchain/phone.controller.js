280e99171a9cfe06326d2b9689520ec6af73376f
Charles Julian <charles@launchxd.com>
2019-11-05 14:15:08 -0500

    removed log code

--- src/controllers/chapter-blockchain/phone.controller.js
+++ src/controllers/chapter-blockchain/phone.controller.js
@@ -2,7 +2,6 @@
 
 export default function ChapterBlockchainPhoneController($scope, $timeout, Steps, WizardHandler, $rootScope) {
   $scope.GoToSuccess = function() {
-    $log.info("phone success");
     Steps.clear();
     $rootScope.showMobilePopout = false;
     WizardHandler.wizard("monitor").next();
