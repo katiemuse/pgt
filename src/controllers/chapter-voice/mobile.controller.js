2f29e88aa157ed72cccfb4ff17823a20ee3cafb9
Charles Julian <dhmwebstuff@gmail.com>
2019-12-02 13:08:18 -0500

    removed unnecessary code from mobile controller

--- src/controllers/chapter-voice/mobile.controller.js
+++ src/controllers/chapter-voice/mobile.controller.js
@@ -1,5 +1,5 @@
 'use strict';
-export default function ChapterVoiceMobileController($scope, TopNavbar, Steps, $document, $log) {
+export default function ChapterVoiceMobileController($scope, TopNavbar, Steps, $document) {
   $scope.startVideo = function() {
     const phonePlaceholder = $document[0].querySelector('.phone-placeholder');
     phonePlaceholder.classList.add('hide');
@@ -7,10 +7,5 @@ export default function ChapterVoiceMobileController($scope, TopNavbar, Steps, $
     playButton.classList.add('hide');
     const gifHolder = $document[0].querySelector('.gif-holder');
     gifHolder.classList.remove('hide');
-    const vidSource = $document[0].querySelector('source');
-    const videoElem = $document[0].querySelector('#step-1-video');
-    videoElem.play();
-    vidSource.setAttribute("src", "../../assets/images/chapters/chapter-mobile/ch4-mobile-cut@60fps.mp4");
-    $log.info("vid source", vidSource);
   };
 }
