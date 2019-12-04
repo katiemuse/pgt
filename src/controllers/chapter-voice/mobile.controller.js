c83e3bcd7b0ac8794d84eeed26674e4c43411d63
charlesjuliandev <charlesjuliandev@users.noreply.github.com>
2019-12-04 14:58:30 -0500

    Sgt 111 continue cta functionality (#185)
    * added controllers to handle scroll to top on continue clicks, added extra functions to handle not scrolling to top on video and gif play
    
    * add scrolling functions to controllers to scroll to the top of page on opening also added playbutton and placeholder to chapter mobile when  video stops playing and added playsinline for mobile ios
    
    * moved variables outside functions to be used by all functions

--- src/controllers/chapter-voice/mobile.controller.js
+++ src/controllers/chapter-voice/mobile.controller.js
@@ -1,19 +1,18 @@
 'use strict';
 export default function ChapterVoiceMobileController($scope, TopNavbar, Steps, $document, $log, $timeout) {
+  const mobileContainer = $document[0].querySelector('.container-fluid.main.mobile');
+  const phonePlaceholder = $document[0].querySelector('.phone-placeholder');
+  const playButton = $document[0].querySelector('.play-button');
+  const gifHolder = $document[0].querySelector('.gif-holder');
   $scope.scrollToTop = function() {
-    const mobileContainer = $document[0].querySelector('.container-fluid.main.mobile');
     if (!mobileContainer.classList.contains("scrolled")) {
       $document[0].querySelector('.container-fluid.main.mobile .navbar-header').scrollIntoView();
     }
   };
   $scope.startVideo = function() {
-    const mobileContainer = $document[0].querySelector('.container-fluid.main.mobile');
     mobileContainer.classList.add('scrolled');
-    const phonePlaceholder = $document[0].querySelector('.phone-placeholder');
     phonePlaceholder.classList.add('hide');
-    const playButton = $document[0].querySelector('.play-button');
     playButton.classList.add('hide');
-    const gifHolder = $document[0].querySelector('.gif-holder');
     gifHolder.classList.remove('hide');
     $timeout(() => {
       const transitionElem = $document[0].querySelector('.slide-1 .transition-copy');
@@ -21,7 +20,6 @@ export default function ChapterVoiceMobileController($scope, TopNavbar, Steps, $
     }, 9000);
   };
   $scope.$on("$destroy", () => {
-    const mobileContainer = $document[0].querySelector('.container-fluid.main.mobile');
     mobileContainer.classList.remove("scrolled");
   });
 }
