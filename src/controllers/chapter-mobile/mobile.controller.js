9929ce63e89ff391d3429475aab96d472cdef386
charlesjuliandev <charlesjuliandev@users.noreply.github.com>
2019-12-04 14:11:33 -0500

    Sgt 111 continue cta functionality (#184)
    * added controllers to handle scroll to top on continue clicks, added extra functions to handle not scrolling to top on video and gif play
    
    * add scrolling functions to controllers to scroll to the top of page on opening also added playbutton and placeholder to chapter mobile when  video stops playing and added playsinline for mobile ios

--- src/controllers/chapter-mobile/mobile.controller.js
+++ src/controllers/chapter-mobile/mobile.controller.js
@@ -1,17 +1,30 @@
 'use strict';
 export default function ChapterMobileMobileController($scope, TopNavbar, Steps, $document, $timeout) {
+  const mobileContainer = $document[0].querySelector('.container-fluid.main.mobile');
+  const phonePlaceholder = $document[0].querySelector('.phone-placeholder');
+  const playButton = $document[0].querySelector('.play-button');
+  const videoElem = $document[0].querySelector('#step-1-video-mobile');
+  const transitionElem = $document[0].querySelector('.slide-0 .transition-copy');
+  $scope.scrollToTop = function() {
+    if (!mobileContainer.classList.contains("scrolled")) {
+      $document[0].querySelector('.container-fluid.main.mobile .navbar-header').scrollIntoView();
+    }
+  };
   $scope.startVideo = function() {
-    const phonePlaceholder = $document[0].querySelector('.phone-placeholder');
-    const playButton = $document[0].querySelector('.play-button');
+    mobileContainer.classList.add('scrolled');
     playButton.classList.add('hide');
-    const videoElem = $document[0].querySelector('#step-1-video-mobile');
     phonePlaceholder.classList.add('hide');
     videoElem.play();
     videoElem.onended = function() {
       $timeout(() => {
-        const transitionElem = $document[0].querySelector('.slide-0 .transition-copy');
         transitionElem.scrollIntoView({behavior: "smooth"});
+        playButton.classList.remove('hide');
+        phonePlaceholder.classList.remove('hide');
       }, 2000);
     };
   };
+  $scope.$on("$destroy", () => {
+    const mobileContainer = $document[0].querySelector('.container-fluid.main.mobile');
+    mobileContainer.classList.remove("scrolled");
+  });
 }
--- src/controllers/chapter-voice/mobile.controller.js
+++ src/controllers/chapter-voice/mobile.controller.js
@@ -1,11 +1,27 @@
 'use strict';
-export default function ChapterVoiceMobileController($scope, TopNavbar, Steps, $document) {
+export default function ChapterVoiceMobileController($scope, TopNavbar, Steps, $document, $log, $timeout) {
+  $scope.scrollToTop = function() {
+    const mobileContainer = $document[0].querySelector('.container-fluid.main.mobile');
+    if (!mobileContainer.classList.contains("scrolled")) {
+      $document[0].querySelector('.container-fluid.main.mobile .navbar-header').scrollIntoView();
+    }
+  };
   $scope.startVideo = function() {
+    const mobileContainer = $document[0].querySelector('.container-fluid.main.mobile');
+    mobileContainer.classList.add('scrolled');
     const phonePlaceholder = $document[0].querySelector('.phone-placeholder');
     phonePlaceholder.classList.add('hide');
     const playButton = $document[0].querySelector('.play-button');
     playButton.classList.add('hide');
     const gifHolder = $document[0].querySelector('.gif-holder');
     gifHolder.classList.remove('hide');
+    $timeout(() => {
+      const transitionElem = $document[0].querySelector('.slide-1 .transition-copy');
+      transitionElem.scrollIntoView({behavior: "smooth"});
+    }, 9000);
   };
+  $scope.$on("$destroy", () => {
+    const mobileContainer = $document[0].querySelector('.container-fluid.main.mobile');
+    mobileContainer.classList.remove("scrolled");
+  });
 }
